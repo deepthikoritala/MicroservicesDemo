38c845ade9b6aad2649d3e574a43dadd93a649de
Deepthi koritala <deepthi.koritala@wisetechglobal.com>
2022-08-29 23:17:38 +0530

    refactoring

--- MicroServicesTestApp/Client/Controllers/ClientController.cs
+++ MicroServicesTestApp/Client/Controllers/ClientController.cs
@@ -1,21 +1,9 @@
+using Common.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
 
 namespace Client.Controllers
 {
-    public class Order
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        public string Description { get; set; }
-
-        public double Total { get; set; }
-
-        public int NoOfItems { get; set; }
-    }
-
 
     [ApiController]
     [Route("[controller]")]
@@ -30,14 +18,14 @@ namespace Client.Controllers
         }
 
         [HttpGet("GetOrders", Name = "GetOrders")]
-        public IEnumerable<Order> GetOrders()
+        public IEnumerable<OrderDTO> GetOrders()
         {
 
             return getOrdersAsync().Result;
         }
 
-        private async Task<List<Order>> getOrdersAsync() {
-            List<Order> orders = new List<Order>();
+        private async Task<List<OrderDTO>> getOrdersAsync() {
+            List<OrderDTO> orders = new List<OrderDTO>();
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:7234");
@@ -47,7 +35,7 @@ namespace Client.Controllers
                 HttpResponseMessage response = await client.GetAsync("/apigateway/Orders/GetOrders");
                 if (response.IsSuccessStatusCode)
                 {
-                    orders = await response.Content.ReadFromJsonAsync<List<Order>>();
+                    orders = await response.Content.ReadFromJsonAsync<List<OrderDTO>>();
                     //Console.WriteLine("Id:{0}\tName:{1}", department.DepartmentId, department.DepartmentName);
                     //Console.WriteLine("No of Employee in Department: {0}", department.Employees.Count);
                 }
---
+++ MicroServicesTestApp/Common/Models/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Models
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Total { get; set; }
+
+        public int NoOfItems { get; set; }
+    }
+
+}
